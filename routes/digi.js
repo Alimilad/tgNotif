16b9400de754352c8dcd93a33c68eda39a8c9b4c
dotnethelp <65740469+dotnethelp@users.noreply.github.com>
2024-03-08 01:10:37 +0330

    starknet

--- routes/digi.js
+++ routes/digi.js
@@ -2,6 +2,7 @@ var express = require('express');
 var digi = require('../services/digi');
 var router = express.Router();
 const { Web3 } = require('web3');
+import { RpcProvider, Call, validateAndParseAddress } from 'starknet'
 
 const ethereumRpcLink = 'https://eth1.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/';
 const starknetRpcLink = 'https://rpc.starknet.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/';
@@ -17,10 +18,20 @@ router.all('/:year?/:month?/:day?', async function (req, res, next) {
             res.json(await digi.notif(query));
         }
         const web3Ethereum = new Web3(ethereumRpcLink);
-        const web3Starknet = new Web3(starknetRpcLink);
         const gasPrice = await web3Ethereum.eth.getGasPrice();
-        const starknetBalance = await web3Starknet.eth.getBalance('0x029E3B6879c6d7Dc2dFAf851dFDBBfa285349cEF66FF894Ed96a0f96C5eA1fB9');
         console.log(gasPrice);
+
+        const provider = new RpcProvider({
+            nodeUrl: starknetRpcLink,
+        });
+
+        // Call the contract to get the account's balance
+        const balanceCall = {
+            contractAddress: '0x029E3B6879c6d7Dc2dFAf851dFDBBfa285349cEF66FF894Ed96a0f96C5eA1fB9',
+            entrypoint: 'balanceOf',
+            calldata: [accountContract],
+        }
+        const starknetBalance = await provider.callContract(balanceCall);
         console.log(starknetBalance);
         //res.json(await digi.notif(req.body));
     } catch (err) {
