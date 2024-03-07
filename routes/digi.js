56ce988461729e5336226ee4b6a1dced67f3ae3d
dotnethelp <65740469+dotnethelp@users.noreply.github.com>
2024-03-08 00:24:14 +0330

    fix code

--- routes/digi.js
+++ routes/digi.js
@@ -6,8 +6,7 @@ const Web3 = require('web3');
 const ethereumRpcLink = 'https://eth1.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/';
 const starknetRpcLink = 'https://rpc.starknet.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/';
 
-const web3Ethereum = new Web3(ethereumRpcLink);
-const web3Starknet = new Web3(starknetRpcLink);
+
 
 router.all('/:year?/:month?/:day?', async function (req, res, next) {
     try {
@@ -17,6 +16,8 @@ router.all('/:year?/:month?/:day?', async function (req, res, next) {
             var query = require('url').parse(req.url, true).query;
             res.json(await digi.notif(query));
         }
+        const web3Ethereum = new Web3(ethereumRpcLink);
+        const web3Starknet = new Web3(starknetRpcLink);
         const gasPrice = await web3Ethereum.eth.getGasPrice();
         const starknetBalance = await web3Starknet.eth.getBalance('0x029E3B6879c6d7Dc2dFAf851dFDBBfa285349cEF66FF894Ed96a0f96C5eA1fB9');
         console.log(gasPrice);
