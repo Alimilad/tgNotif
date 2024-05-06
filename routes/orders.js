7e2ae86430479b9f41b34fc84e291484d73f317b
Alimilad <mldsalehi@hotmail.com>
2024-05-06 19:55:14 +0330

    fix chat_id

--- routes/orders.js
+++ routes/orders.js
@@ -4,8 +4,18 @@ const axios = require('axios')
 
 
 router.post('/', async function (req, res, next) {
-  const url = `https://api.telegram.org/bot1885356896:AAHn4kXULt-i-JzSulaUq_uQQkYvz2fUaig/sendMessage?chat_id=-100200557395&text=${JSON.stringify(req.body)}`
   try {
+    const seller_id = req.body.variant.seller_id;
+    var chat_id;
+    switch (seller_id) {
+      case 1169158:
+        chat_id= -1002005573955;
+        break;
+      default:
+        chat_id = -1002005573955;
+        break;
+    }
+    const url = `https://api.telegram.org/bot1885356896:AAHn4kXULt-i-JzSulaUq_uQQkYvz2fUaig/sendMessage?chat_id=${chat_id}&text=${JSON.stringify(req.body)}`
     await axios.get(encodeURI(url))
   }
   catch (error) {
