f92f34d3f8cdb030a3aee27a48881bfc1dcf0583
Alimilad <mldsalehi@hotmail.com>
2024-05-17 20:11:45 +0330

    debug

--- routes/wallet.js
+++ routes/wallet.js
@@ -6,14 +6,16 @@ const { Web3 } = require('web3');
 const ETHRpcUrl = 'https://eth1.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/'; // Replace with your RPC URL
 
 router.post('/', async function (req, res, next) {
-  const url1 = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=hggggg${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
-  await axios.get(encodeURI(url1))
   try {
+    const url1 = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=1${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
+    await axios.get(encodeURI(url1))
      const web3 = new Web3(new Web3.providers.HttpProvider(ETHRpcUrl));
-     res.json(web3);
+     const url2 = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=2${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
+     await axios.get(encodeURI(url2))
      //Get the balance in Wei
       const balanceWei = await web3.eth.getBalance(req.body.address);
-      res.json(balanceWei);
+      const url3 = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=3${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
+      await axios.get(encodeURI(url3))
       // Convert the balance from Wei to Ether
       balanceEth = web3.utils.fromWei(balanceWei, 'ether');
       var message = `balance : ${balanceEth} \n address : ${req.body.address}`;
