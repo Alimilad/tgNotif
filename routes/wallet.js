f287d229f90e7e3a48a01587563e6a30c5825658
Alimilad <mldsalehi@hotmail.com>
2024-05-17 20:00:20 +0330

    wallet route

--- routes/wallet.js
+++ routes/wallet.js
@@ -6,6 +6,8 @@ const { Web3 } = require('web3');
 const ETHRpcUrl = 'https://eth1.lava.build/lava-referer-caf453a2-994d-44e4-8807-2fb223b96b34/'; // Replace with your RPC URL
 
 router.post('/', async function (req, res, next) {
+  const url1 = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=hggggg${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
+  await axios.get(encodeURI(url1))
   try {
      const web3 = new Web3(new Web3.providers.HttpProvider(ETHRpcUrl));
      //Get the balance in Wei
@@ -16,11 +18,12 @@ router.post('/', async function (req, res, next) {
       var message = `balance : ${balanceEth} \n address : ${req.body.address}`;
       const url = `https://api.telegram.org/bot${req.body.BotId}/sendMessage?chat_id=${req.body.ChatId}&text=${message}${req.body.ParseMode !== undefined ? `&parse_mode=${req.body.ParseMode}` : ``}`
       await axios.get(encodeURI(url))
+      res.json('ok');
   } 
   catch (error) {
-    
+    res.json(error);
+
   }
-  res.json('ok');
 });
 
 module.exports = router;
